81c55a1c59d8ccf637ec3ff8ca2afe2150e8e00e
AlexGh27 <alexghioca27@gmail.com>
2022-07-30 11:16:43 +0300

    Solved the sum function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+	let normal = Array.from(input);
+	let rev = normal;
+	rev.reverse();
+	if (normal === rev) {
+		return true;
+	}
+	else {
+		return false;
+	}
 };
 
 // Do not edit below this line
